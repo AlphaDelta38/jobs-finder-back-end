0a27d1441c6ac765e6e562c1e7cbcc6e4a98220f
AlphaDelta38 <kiril.morozov.01@gmail.com>
2025-05-29 22:24:48 +0300

    fix #3

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 const allowedOrigins = [
+    'https://jobs-finder-front-end-puob.vercel.app',
     'https://jobs-finder-front-end-puob-mu824ck8g-kirils-projects-63149d58.vercel.app',
     'http://localhost:3000'
 ];
